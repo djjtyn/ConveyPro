853e202a797fc869007043285f9ff58b2fc4363b
Dave Tynan <djjtynan@gmail.com>
2024-11-15 16:40:09 +0000

    user auth via email

--- Auth/models.py
+++ Auth/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 class Company(models.Model):
@@ -13,10 +13,34 @@ class UserType(models.Model):
     def __str__(self):
         return self.type
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        # Ensure the user is a superuser and staff
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+    def get_by_natural_key(self, email):
+        # Return a user by email (since email is the natural key now)
+        return self.get(email=email)
+
 #Custom User model which extends AbstractUser class to allow additional attributes
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
     company = models.ForeignKey('Company', on_delete = models.SET_NULL, null = True)
     user_type = models.ForeignKey('UserType', on_delete = models.SET_NULL, null = True)
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.username
+        return self.email
